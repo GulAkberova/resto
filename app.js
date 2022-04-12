48cb72f00cdb879ea0573020bf158f314c3b54ac
unknown <GulAkberova>
2022-04-12 18:56:07 +0400

    resto

---
+++ app.js
@@ -0,0 +1,9 @@
+let img=document.getElementsByTagName("img")
+let btn1=document.getElementById("btn1")
+let btn2=document.getElementById("btn2")
+let index=0
+btn2.addEventListener("click",()=>{
+  document.getElementsByClassName("show")[0].classList.remove[0];
+  index++;
+  img[index].classList.add("show");
+})
